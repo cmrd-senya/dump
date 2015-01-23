2f5df7597133e181866f7cc524b146bd556cc03d
Martin Beck <mail@martinbeck.net>
2015-01-23 17:49:40 +0100

    removed unnecessary code

--- lib/dump/capistrano/v3.rb
+++ lib/dump/capistrano/v3.rb
@@ -11,28 +11,6 @@ require 'active_support/core_ext/object/blank'
 require 'English'
 
 namespace :dump do
-  def dump_command(command, env = {})
-    rake = env.delete(:rake) || 'rake'
-
-    # stringify_keys! from activesupport
-    Dump::Env.stringify!(env)
-
-    env.update(Dump::Env.for_command(command, true))
-
-    cmd = %W[-s dump:#{command}]
-    cmd += env.sort.map{ |key, value| "#{key}=#{value}" }
-    "#{rake} #{cmd.shelljoin}"
-  end
-
-  def fetch_rails_env
-    fetch(:rails_env, 'production')
-  end
-
-  def got_rsync?
-    `which rsync`
-    $CHILD_STATUS.success?
-  end
-
   def with_additional_tags(*tags)
     tags = [tags, Dump::Env[:tags]].flatten.select(&:present?).join(',')
     Dump::Env.with_env(:tags => tags) do
@@ -47,24 +25,7 @@ namespace :dump do
     out.strip
   end
 
-  def run_local(cmd)
-    `#{cmd}`
-  end
-
-  def run_remote(cmd)
-    output = ''
-    run(cmd) do |_channel, io, data|
-      case io
-      when :out
-        output << data
-      when :err
-        $stderr << data
-      end
-    end
-    output
-  end
-  
-  def capture_local(rake_task, env = {})
+  def run_local(rake_task, env = {})
     run_locally do
       with(env) do
         capture(:rake, "dump:#{rake_task}")
@@ -72,7 +33,7 @@ namespace :dump do
     end
   end
   
-  def capture_remote(rake_task, env = {})
+  def run_remote(rake_task, env = {})
     output = nil
     on primary(:db) do
       within current_path do
@@ -98,30 +59,30 @@ namespace :dump do
     task :create do
       print_and_return_or_fail do
         with_additional_tags('local') do
-          capture_local(:create)
+          run_local(:create)
         end
       end
     end
 
     desc 'Restore local dump' << Dump::Env.explain_variables_for_command(:restore)
     task :restore do
-      capture_local(:restore)
+      run_local(:restore)
     end
 
     desc 'Versions of local dumps' << Dump::Env.explain_variables_for_command(:versions)
     task :versions do
-      puts capture_local(:versions, show_size: true)
+      puts run_local(:versions, show_size: true)
     end
 
     desc 'Cleanup local dumps' << Dump::Env.explain_variables_for_command(:cleanup)
     task :cleanup do
-      puts capture_local(:cleanup)
+      puts run_local(:cleanup)
     end
 
     desc 'Upload dump' << Dump::Env.explain_variables_for_command(:transfer)
     task :upload do
       file = Dump::Env.with_env(:summary => nil) do
-        last_part_of_last_line(capture_local(:versions))
+        last_part_of_last_line(run_local(:versions))
       end
       if file
         on primary(:db) do
@@ -136,30 +97,30 @@ namespace :dump do
     task :create do
       print_and_return_or_fail do
         with_additional_tags('remote') do
-          capture_remote(:create)
+          run_remote(:create)
         end
       end
     end
     
     desc 'Restore remote dump' << Dump::Env.explain_variables_for_command(:restore)
     task :restore do
-      capture_remote(:restore)
+      run_remote(:restore)
     end
     
     desc 'Versions of remote dumps' << Dump::Env.explain_variables_for_command(:versions)
     task :versions do
-      puts capture_remote(:versions, show_size: true)
+      puts run_remote(:versions, show_size: true)
     end
     
     desc 'Cleanup of remote dumps' << Dump::Env.explain_variables_for_command(:cleanup)
     task :cleanup do
-      puts capture_remote(:cleanup)
+      puts run_remote(:cleanup)
     end
     
     desc 'Download dump' << Dump::Env.explain_variables_for_command(:transfer)
     task :download do
       file = Dump::Env.with_env(:summary => nil) do
-        last_part_of_last_line(capture_remote(:versions))
+        last_part_of_last_line(run_remote(:versions))
       end
       if file
         FileUtils.mkpath('dump')
@@ -192,7 +153,7 @@ namespace :dump do
       end
       if !auto_backup? || auto_backup.present?
         file = with_additional_tags('mirror') do
-          capture_local(:create)
+          run_local(:create)
         end
         if file.present?
           Dump::Env.with_clean_env(:like => file) do
@@ -212,7 +173,7 @@ namespace :dump do
       end
       if !auto_backup? || auto_backup.present?
         file = with_additional_tags('mirror') do
-          capture_remote(:create)
+          run_remote(:create)
         end
         if file.present?
           Dump::Env.with_clean_env(:like => file) do
@@ -228,7 +189,7 @@ namespace :dump do
     desc "Creates remote dump and downloads to local (desc defaults to 'backup')" << Dump::Env.explain_variables_for_command(:backup)
     task :create do
       file = with_additional_tags('backup') do
-        capture_remote(:create)
+        run_remote(:create)
       end
       if file.present?
         Dump::Env.with_clean_env(:like => file) do
@@ -240,7 +201,7 @@ namespace :dump do
     desc 'Uploads dump with backup tag and restores it on remote' << Dump::Env.explain_variables_for_command(:backup_restore)
     task :restore do
       file = with_additional_tags('backup') do
-        last_part_of_last_line(capture_local(:versions))
+        last_part_of_last_line(run_local(:versions))
       end
       if file.present?
         Dump::Env.with_clean_env(:like => file) do
